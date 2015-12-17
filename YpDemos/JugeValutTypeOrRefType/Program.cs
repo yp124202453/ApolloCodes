5265dd090800a6199752af35c0a988d3103df5de
freedomjing000 <124202453@qq.com>
2015-12-17 10:05:22 +0800

    add Project YpDemos

---
+++ YpDemos/JugeValutTypeOrRefType/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JugeValutTypeOrRefType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type t1 = typeof(int);
+            if (t1.IsValueType)
+            {
+                Console.WriteLine("{0} is ValueType", t1.FullName);
+            }
+            else
+            {
+                Console.WriteLine("{0} is refference type", t1.FullName);
+            }
+
+            Type t2 = typeof(ValueType);
+            if (t2.IsValueType)
+            {
+                Console.WriteLine("{0} is ValueType", t2.FullName);
+            }
+            else
+            {
+                Console.WriteLine("{0} is refference type", t2.FullName);
+            }
+
+            Console.WriteLine("press any key to exit......");
+            Console.ReadKey();
+        }
+    }
+}
